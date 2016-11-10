2bfc2b7c64d8a7936e5e098c84954ef43b7404d7
Sirozha1337 <schernyae@gmail.com>
2016-11-10 16:12:55 +0300

    New sounds implemented, changes to player animation

--- Assets/Scripts/SoundEffectsHelper.cs
+++ Assets/Scripts/SoundEffectsHelper.cs
@@ -2,52 +2,79 @@
 using System.Collections;
 
 public class SoundEffectsHelper : MonoBehaviour {
-	public AudioClip jumpingSound;
-	public AudioClip fallFromJumpSound;
+	// Add sound effects
+	public AudioClip[] jumpingSound;
+	public AudioClip[] fallFromJumpSound;
 	public AudioClip movingSound;
+	public AudioClip musicTheme;
 
-	private AudioSource source;
-	private AudioSource source2;
-	private AudioClip currentSound;
+	// Control the volume 
+	public float volumeJumpFall;
+	public float volumeTheme;
+
+	// Play sounds
+	private AudioSource characterJumpFall;
+	private AudioSource characterMove;
+	private AudioSource theme;
+
+	// This is for checking if player is moving
 	private GameObject player;
+
+	// Returns a new audiosource
+	AudioSource AddAudio(bool loop, bool playAwake, float vol) { 
+     	AudioSource newAudio = gameObject.AddComponent<AudioSource>();
+     	newAudio.loop = loop;
+     	newAudio.playOnAwake = playAwake;
+     	newAudio.volume = vol; 
+     	return newAudio; 
+	}
 
 	// Use this for initialization
 	void Start () {
-		source = GetComponent<AudioSource>();
-		source2 = gameObject.AddComponent<AudioSource>();
-		source2.clip = movingSound;
-		source2.loop = true;
-		source2.playOnAwake = true;
-		source2.volume = 1;
+		// Initialize Jump and Fall source
+		characterJumpFall = AddAudio(false, true, 0.5f);
+		
+		// Initialize Movement sound source
+		characterMove = AddAudio(true, true, 0.5f);
+		characterMove.clip = movingSound;
+
+		// Initialize music theme source
+		theme = AddAudio(true, true, volumeTheme);
+		theme.clip = musicTheme;
+		theme.Play();
+
+		// Find the player gameObject
 		player = GameObject.FindWithTag("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		// Play moving sound 
 		MovingSound();
 	}
 
 	void JumpingSound(){
-		if(!source.isPlaying || currentSound != jumpingSound && !GameOverScript.IsGameOver){
-			currentSound = jumpingSound;
-			source.PlayOneShot(currentSound, 0.2f);
+		// If it's not Game Over play a sound
+		if(!GameOverScript.IsGameOver){
+			characterJumpFall.PlayOneShot(jumpingSound[Random.Range(0, jumpingSound.Length)], volumeJumpFall);
 		}
 	}
 
 	void FallingSound(){
-		if(!source.isPlaying || currentSound != fallFromJumpSound && !GameOverScript.IsGameOver){
-			currentSound = fallFromJumpSound;
-			source.PlayOneShot(currentSound, 0.2f);
+		// If it's not Game Over play a sound
+		if(!GameOverScript.IsGameOver){
+			characterJumpFall.PlayOneShot(fallFromJumpSound[Random.Range(0, fallFromJumpSound.Length)], volumeJumpFall);
 		}
 	}
 
 	void MovingSound(){
+		// Check if player is moving and play movement sound
 		if(player.GetComponent<Rigidbody2D>().velocity.x > 0 || player.GetComponent<Rigidbody2D>().velocity.x < 0  &&   player.GetComponent<PlayerScript>().isGrounded && !GameOverScript.IsGameOver){
-			if(!source2.isPlaying){
-				source2.Play();
+			if(!characterMove.isPlaying){
+				characterMove.Play();
 			}
 		}else{
-			source2.Pause();
+			characterMove.Pause();
 		}
 	}
 }
