c26af705000e897fcd729c31aa33aacb2c0d98d3
imvsidorko <rucamper@mail.ru>
2017-04-20 13:52:30 +0300

    * Добавлен спрайт * Добавлен новый вид активаторов - TriggerArea * Небольшие тесты с Dialoger'ом

--- Assets/Scripts/Dialoger.cs
+++ Assets/Scripts/Dialoger.cs
@@ -7,28 +7,37 @@ using UnityEngine.UI;
 public class Dialoger : MonoBehaviour
 {
 	private GameObject playerGO;
-	
+
+
     public Transform[] SpawnLocation;
     public GameObject[] ObjPrefab;
+    public string[] Message;
+    private int[] TimeToDestroy;
 	
 	private List<GameObject> ObjClone;
 	
-    // Use this for initialization
+    void Start () {
+
+	}
 
     void Activate()
     {
-        SpawnSomething();
+        SpawnSomethingTest();
     }
-
+    void SpawnSomethingTest()
+    {
+        ObjClone = new List<GameObject>();
+        ObjClone.Add(Instantiate(ObjPrefab[0], SpawnLocation[0].transform.position, Quaternion.Euler(0, 0, 0)) as GameObject);
+        ObjClone[ObjClone.Count - 1].GetComponentInChildren<TextMesh>().text = Message[ObjClone.Count - 1];
+        //TimeToDestroy[ObjClone.Count - 1] = 600;
+        playerGO = GameObject.FindGameObjectsWithTag("Player")[0];
+    }
     void SpawnSomething()
     {
 		ObjClone = new List<GameObject>();
-	
+
         ObjClone.Add(Instantiate(ObjPrefab[0], SpawnLocation[0].transform.position, Quaternion.Euler(0, 0, 0)) as GameObject);
         ObjClone.Add(Instantiate(ObjPrefab[1], SpawnLocation[1].transform.position, Quaternion.Euler(0, 0, 0)) as GameObject);
-
-        //ObjClone[1].AddComponent<FY>(); // Че, как это вообще использовать, че за хрень?
-        ObjClone.Add(Instantiate(ObjPrefab[2], SpawnLocation[2].transform.position, Quaternion.Euler(0, 0, 0)) as GameObject);
 
 		ObjClone[0].GetComponentInChildren<TextMesh>().text = "new text";
 		
@@ -40,20 +49,8 @@ public class Dialoger : MonoBehaviour
     {
 		if(null == ObjClone)
 			return;
-		
-		ObjClone[0].transform.position = playerGO.transform.position;
-    }
-
-    void SpawnCubes()
-    {
-        for (int y = 0; y < 5; y++)
-        {
-            for (int x = 0; x < 5; x++)
-            {
-                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                cube.AddComponent<Rigidbody>();
-                cube.transform.position = new Vector3(x / 2, y / 2, 0);
-            }
-        }
+        ObjClone[0].transform.position = playerGO.transform.position;
+        /*if (TimeToDestroy[0] > 0) { TimeToDestroy[0] -= 1; ObjClone[0].transform.position = playerGO.transform.position; }
+        else Destroy(ObjClone[0]);*/
     }
 }
