b4e6726096c51b21473cf038c8452b6489ccce19
meprohor <meprohor@gmail.com>
2017-04-27 13:59:12 +0400

    Saving is presumable implemented, loading is not though

---
+++ Assets/Scripts/dataManagement/Level.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+public class Level : MonoBehaviour
+{
+	void Awake()
+	{
+		string tempS = SceneManager.GetActiveScene().name;
+		
+		if(!Game.unlockedLevels.Contains(tempS))
+			Game.unlockedLevels.Add(tempS);
+		
+		Game.Save();
+	}
+}
