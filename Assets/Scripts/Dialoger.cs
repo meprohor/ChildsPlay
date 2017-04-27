1ff64c011ee29bc7edc80d94fa4a2ed6cb2b73cd
imvsidorko <rucamper@mail.ru>
2017-04-27 10:56:33 +0300

    Практически доделаны диалоговые окна

--- Assets/Scripts/Dialoger.cs
+++ Assets/Scripts/Dialoger.cs
@@ -6,51 +6,82 @@ using UnityEngine.UI;
 
 public class Dialoger : MonoBehaviour
 {
-	private GameObject playerGO;
+    // Для создания объекта (ПОЛЯ СКРИПТА)
+    public List<Transform> SpawnLocation;
+    public List<GameObject> ObjPrefab;
+    public List<string> Message;
+    public List<int> TimeToDestroy;
+    public List<bool> IsPlayerParent;
 
+    /*public class OneMessage
+    {
+        public Transform SpawnLocation;
+        public GameObject ObjPrefab;
+        public string Message;
+        public int TimeToDestroy;
+        public bool IsPlayerParent;
+    }
 
-    public Transform[] SpawnLocation;
-    public GameObject[] ObjPrefab;
-    public string[] Message;
-    private int[] TimeToDestroy;
+    public List<OneMessage> InputObj;*/
+
+    // Ссылка на объект игрока (Чтобы к нему крепить сообщения)
+    private GameObject playerGO;
+    // Чтобы знать какое сообщения мы показывали в последний раз
+    private int CurrentShowMessage = -1;
+    // Объекты, которые будем выводить и прочее
+    private List<GameObject> ObjClone = new List<GameObject>();
 	
-	private List<GameObject> ObjClone;
-	
+
     void Start () {
-
+        // Находим ОБЪЕКТ игрока чтобы к нему можно было прицепить текст
+        playerGO = GameObject.FindGameObjectsWithTag("Player")[0];
+        // Переносы текста
+        /*foreach (string item in Message)
+        {
+            if (item.Length > 4) item.Insert(4, "\n");
+        }*/
 	}
 
     void Activate()
     {
-        SpawnSomethingTest();
+        ShowNextMessage();
     }
-    void SpawnSomethingTest()
+    void ShowNextMessage()
     {
-        ObjClone = new List<GameObject>();
-        ObjClone.Add(Instantiate(ObjPrefab[0], SpawnLocation[0].transform.position, Quaternion.Euler(0, 0, 0)) as GameObject);
-        ObjClone[ObjClone.Count - 1].GetComponentInChildren<TextMesh>().text = Message[ObjClone.Count - 1];
-        //TimeToDestroy[ObjClone.Count - 1] = 600;
-        playerGO = GameObject.FindGameObjectsWithTag("Player")[0];
+        // Если показаны все прописанные фразу - ничего не выводить
+        if (CurrentShowMessage < ObjPrefab.Count - 1) CurrentShowMessage++; else return;
+        // Добавляем инфу о новом сообщении
+        ObjClone.Add(Instantiate(ObjPrefab[CurrentShowMessage], SpawnLocation[CurrentShowMessage].transform.position, Quaternion.Euler(0, 0, 0)) as GameObject);
+        // Устанавливаем новый текст объекту
+        ObjClone[CurrentShowMessage].GetComponentInChildren<TextMesh>().text = Message[CurrentShowMessage]; 
     }
-    void SpawnSomething()
+
+    /*void SpawnSomething()
     {
 		ObjClone = new List<GameObject>();
 
         ObjClone.Add(Instantiate(ObjPrefab[0], SpawnLocation[0].transform.position, Quaternion.Euler(0, 0, 0)) as GameObject);
         ObjClone.Add(Instantiate(ObjPrefab[1], SpawnLocation[1].transform.position, Quaternion.Euler(0, 0, 0)) as GameObject);
 
-		ObjClone[0].GetComponentInChildren<TextMesh>().text = "new text";
+        ObjClone[0].GetComponentInChildren<TextMesh>().text = Message[0];
 		
 		playerGO = GameObject.FindGameObjectsWithTag("Player")[0];
-    }
+    }*/
 
     // Update is called once per frame
     void Update()
     {
-		if(null == ObjClone)
-			return;
-        ObjClone[0].transform.position = playerGO.transform.position;
-        /*if (TimeToDestroy[0] > 0) { TimeToDestroy[0] -= 1; ObjClone[0].transform.position = playerGO.transform.position; }
-        else Destroy(ObjClone[0]);*/
+        // Бежим по всем объектам и приближаем их к смерти(!)
+        for (int i = 0; i < ObjClone.Count; i++)
+            if (TimeToDestroy[i] > 0)
+            {
+                // ДЕинкрементируем время то самоуничтожения
+                TimeToDestroy[i] -= 1;
+                // Следует за игроком только если это необходимо
+                if (IsPlayerParent[i]) ObjClone[i].transform.position = playerGO.transform.position;
+            }
+            else
+                Destroy(ObjClone[i]);
+        //ObjClone[0].GetComponentInChildren<TextMesh>().text = Message[0] + TimeToDestroy[0].ToString(); // DEBUG
     }
 }
